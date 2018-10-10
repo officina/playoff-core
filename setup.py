30d455566cfd9c9941d2db1ab31be631a84e23f3
Mattia <mattia21bano@gmail.com>
2018-10-10 12:08:02 +0200

    Version change

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="playoff_core",
-    version="0.8.0",
+    version="0.8.1",
     author="Mattia Bano",
     author_email="dev@officina.cc",
     description="Playoff toolbox",
