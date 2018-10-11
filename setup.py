1066b476bdd84bd88119c056974184f803d52e6f
Mattia <mattia21bano@gmail.com>
2018-10-11 14:59:03 +0200

    Added simple import usage

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="playoff_core",
-    version="0.8.2",
+    version="0.8.3",
     author="Mattia Bano",
     author_email="dev@officina.cc",
     description="Playoff toolbox",
@@ -23,5 +23,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
+    ]
 )
